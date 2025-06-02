34a7547a9cbba3913eee85455efc0f2e4718bff6
Dan <betancourtdaniel90@gmail.com>
2025-06-02 01:40:31 -0700

    Added tailWind and verrified rendering

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+
+function App() {
+  return (
+    <div className="bg-blue-100 text-2xl font-bold text-center p-4">
+      Book Club Dashboard is Live 🚀
+    </div>
+  );
+}
+
+
+export default App;
